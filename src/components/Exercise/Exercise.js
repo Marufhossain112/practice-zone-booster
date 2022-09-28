db477174b11583967a6a31e2384227036c125d1f
maruf <lancerboy284@gmail.com>
2022-09-29 00:25:32 +0600

    left side markup done

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Exercise.css";
 const Exercise = (props) => {
-  const { img, age, headerTitle, id, instruction, requiredTime } = props.info;
+  const { img, age, headerTitle, instruction, requiredTime } = props.info;
   return (
     <div className="player-info">
       <img src={img} alt="" />
-      <p>age : {age}</p>
+      <h3>{headerTitle}</h3>
+      <p>{instruction}</p>
+      <p>For Age : {age}</p>
+      <p>Time required : {requiredTime}</p>
+      <button>Add to list</button>
     </div>
   );
 };
