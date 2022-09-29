e35f84db8a1d9bf2c69101bbcc62c8f15d9797eb
maruf <lancerboy284@gmail.com>
2022-09-29 21:01:09 +0600

    Break time box problem  fixed

--- src/components/Break-duration/BreakDuration.js
+++ src/components/Break-duration/BreakDuration.js
@@ -6,25 +6,19 @@ const BreakDuration = (props) => {
   //   console.log(props);
   const notify = () => toast("Congratulations , Activity Completed !");
   const { breaks, onChange } = props;
-  const inputStyle = {
-    outline: 0,
-    border: 0,
-  };
-  const divStyle = {
-    border: "1px solid gray",
-    display: "inline",
-    padding: "2px",
-  };
+
   return (
-    <div style={divStyle}>
-      <span>Break time</span>
-      <input
-        style={inputStyle}
-        type="text"
-        className="mt-2 p-2"
-        onChange={onChange}
-        value={breaks}
-      />
+    <div>
+      <div className="flex flex-col">
+        <span>Break time</span>
+        <input
+          type="text"
+          className="mt-2 mx-5 p-2 "
+          onChange={onChange}
+          value={breaks}
+        />
+      </div>
+
       <button
         className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-4"
         onClick={notify}
